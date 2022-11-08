474193c316bd846404e71ffe04da8465954aab25
usfit <dima050595fedorov@yandex.ru>
2022-11-08 20:04:06 +0300

    start hooks

--- src/NewTaskForm/NewTaskForm.js
+++ src/NewTaskForm/NewTaskForm.js
@@ -1,100 +1,154 @@
 /* eslint-disable jsx-a11y/no-autofocus */
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-class NewTaskForm extends Component {
-  static defaultProps = {
-    className: '',
-    addNewTask: () => {},
-  };
-
-  static propTypes = {
-    className: PropTypes.string,
-    addNewTask: PropTypes.func,
-  };
-
-  state = {
-    newTaskName: '',
-    newMin: '',
-    newSec: '',
-  };
-
-  changeNameNewTask = (e) => {
-    this.setState(() => {
-      return {
-        newTaskName: e.target.value,
-      };
-    });
-  };
-
-  changeMinNewTask = (e) => {
-    this.setState(() => {
-      return {
-        newMin: e.target.value,
-      };
-    });
-  };
-
-  changeSecNewTask = (e) => {
-    this.setState(() => {
-      return {
-        newSec: e.target.value,
-      };
-    });
-  };
-
-  submitNewTask = (e) => {
-    e.preventDefault();
-    const { addNewTask } = this.props;
-    const { newTaskName, newMin, newSec } = this.state;
-    const correctName = !!(newTaskName.length !== 0 && newTaskName.trim());
-    const correctMin = !!(+newMin + 1 && +newMin >= 0);
-    const correctSec = !!(+newSec && +newSec >= 0 && +newSec < 60);
-    if (correctName && correctMin && correctSec) {
-      addNewTask(newTaskName, newMin, newSec);
-      this.setState(() => {
-        return {
-          newTaskName: '',
-          newMin: '',
-          newSec: '',
-        };
-      });
-    }
-  };
-
-  render() {
-    const { newTaskName, newMin, newSec } = this.state;
-
-    return (
-      <header className="header">
-        <h1>todos</h1>
-        <form onSubmit={this.submitNewTask} className="new-todo-form">
-          <input
-            type="text"
-            className="new-todo"
-            placeholder="What needs to be done?"
-            onChange={this.changeNameNewTask}
-            value={newTaskName}
-          />
-          <input
-            type="text"
-            className="new-todo-form__timer"
-            placeholder="Min"
-            onChange={this.changeMinNewTask}
-            value={newMin}
-          />
-          <input
-            type="text"
-            className="new-todo-form__timer"
-            placeholder="Sec"
-            onChange={this.changeSecNewTask}
-            value={newSec}
-          />
-          <input className="hidden" type="submit" />
-        </form>
-      </header>
-    );
+const submitNewTask = (e, addNewTask, newTaskName, newMin, newSec) => {
+  e.preventDefault();
+  const correctName = !!(newTaskName.length !== 0 && newTaskName.trim());
+  const correctMin = !!(+newMin + 1 && +newMin >= 0);
+  const correctSec = !!(+newSec >= 0 && +newSec < 60);
+  if (correctName && correctMin && correctSec) {
+    addNewTask(newTaskName, newMin, newSec);
   }
+};
+function NewTaskForm({ addNewTask }) {
+  const [newTaskName, setNewTaskName] = useState('');
+  const [newMin, setNewMin] = useState('');
+  const [newSec, setNewSec] = useState('');
+  return (
+    <header className="header">
+      <h1>todos</h1>
+      <form onSubmit={(e) => submitNewTask(e, addNewTask, newTaskName, newMin, newSec)} className="new-todo-form">
+        <input
+          type="text"
+          className="new-todo"
+          placeholder="What needs to be done?"
+          onChange={(e) => setNewTaskName(() => e.target.value)}
+          value={newTaskName}
+        />
+        <input
+          type="text"
+          className="new-todo-form__timer"
+          placeholder="Min"
+          onChange={(e) => setNewMin(() => e.target.value)}
+          value={newMin}
+        />
+        <input
+          type="text"
+          className="new-todo-form__timer"
+          placeholder="Sec"
+          onChange={(e) => setNewSec(e.target.value)}
+          value={newSec}
+        />
+        <input className="hidden" type="submit" />
+      </form>
+    </header>
+  );
 }
+
+NewTaskForm.defaultProps = {
+  className: '',
+  addNewTask: () => {},
+};
+
+NewTaskForm.propTypes = {
+  className: PropTypes.string,
+  addNewTask: PropTypes.func,
+};
+
+// class NewTaskFormClass extends Component {
+//   static defaultProps = {
+//     className: '',
+//     addNewTask: () => {},
+//   };
+
+//   static propTypes = {
+//     className: PropTypes.string,
+//     addNewTask: PropTypes.func,
+//   };
+
+//   state = {
+//     newTaskName: '',
+//     newMin: '',
+//     newSec: '',
+//   };
+
+//   changeNameNewTask = (e) => {
+//     this.setState(() => {
+//       return {
+//         newTaskName: e.target.value,
+//       };
+//     });
+//   };
+
+//   changeMinNewTask = (e) => {
+//     this.setState(() => {
+//       return {
+//         newMin: e.target.value,
+//       };
+//     });
+//   };
+
+//   changeSecNewTask = (e) => {
+//     this.setState(() => {
+//       return {
+//         newSec: e.target.value,
+//       };
+//     });
+//   };
+
+//   submitNewTask = (e) => {
+//     e.preventDefault();
+//     const { addNewTask } = this.props;
+//     const { newTaskName, newMin, newSec } = this.state;
+//     const correctName = !!(newTaskName.length !== 0 && newTaskName.trim());
+//     const correctMin = !!(+newMin + 1 && +newMin >= 0);
+//     const correctSec = !!(+newSec >= 0 && +newSec < 60);
+//     if (correctName && correctMin && correctSec) {
+//       addNewTask(newTaskName, newMin, newSec);
+//       this.setState(() => {
+//         return {
+//           newTaskName: '',
+//           newMin: '',
+//           newSec: '',
+//         };
+//       });
+//     }
+//   };
+
+//   render() {
+//     const { newTaskName, newMin, newSec } = this.state;
+
+//     return (
+//       <header className="header">
+//         <h1>todos</h1>
+//         <form onSubmit={this.submitNewTask} className="new-todo-form">
+//           <input
+//             type="text"
+//             className="new-todo"
+//             placeholder="What needs to be done?"
+//             onChange={this.changeNameNewTask}
+//             value={newTaskName}
+//           />
+//           <input
+//             type="text"
+//             className="new-todo-form__timer"
+//             placeholder="Min"
+//             onChange={this.changeMinNewTask}
+//             value={newMin}
+//           />
+//           <input
+//             type="text"
+//             className="new-todo-form__timer"
+//             placeholder="Sec"
+//             onChange={this.changeSecNewTask}
+//             value={newSec}
+//           />
+//           <input className="hidden" type="submit" />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
 
 export default NewTaskForm;
--- src/Task/Task.js
+++ src/Task/Task.js
@@ -1,95 +1,176 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { formatDistanceToNowStrict } from 'date-fns';
 
 import TaskTimer from '../TaskTimer';
 
-class Task extends Component {
-  static defaultProps = {
-    label: 'New task',
-    completed: false,
-    editing: false,
-    dateCreated: new Date(),
-    deleteTask: () => {},
-    editTask: () => {},
-    editTaskSubmit: () => {},
-    changeCompleted: () => {},
-  };
-
-  static propTypes = {
-    label: PropTypes.string,
-    completed: PropTypes.bool,
-    editing: PropTypes.bool,
-    dateCreated: PropTypes.instanceOf(Date),
-    deleteTask: PropTypes.func,
-    editTask: PropTypes.func,
-    editTaskSubmit: PropTypes.func,
-    changeCompleted: PropTypes.func,
-  };
-
-  state = {
-    newLabel: null,
-  };
-
-  deleteTaskItem = () => {
-    const { deleteTask } = this.props;
-    deleteTask();
-  };
-
-  render() {
-    const { changeCompleted, completed, editing, dateCreated, editTask, editTaskSubmit, minutes, seconds, updateTime } =
-      this.props;
-    let { label } = this.props;
-    const { newLabel } = this.state;
-    const taskTimer = <TaskTimer minutes={minutes} seconds={seconds} updateTime={updateTime} />;
-    return (
-      <li className={[editing ? 'editing' : [completed ? 'completed' : '']]}>
-        <div className="view">
-          <input className="toggle" type="checkbox" onChange={changeCompleted} checked={completed} />
-          <label htmlFor="desctiption">
-            <span className="title" role="presentation" onClick={changeCompleted} onKeyDown={changeCompleted}>
-              {label}
-            </span>
-            {!completed ? taskTimer : null}
-            <span className="description">
-              created {formatDistanceToNowStrict(dateCreated, { includeSeconds: true })} ago
-            </span>
-          </label>
-          <button
-            type="button"
-            aria-label="Edit task"
-            className="icon icon-edit"
-            onClick={(e) => {
-              e.stopPropagation();
-              editTask();
-            }}
-          />
-          <button
-            type="button"
-            aria-label="Delete task"
-            className="icon icon-destroy"
-            onClick={() => this.deleteTaskItem()}
-          />
-        </div>
-        <form onSubmit={(e) => editTaskSubmit(e, newLabel)}>
-          <input
-            type="text"
-            className="edit"
-            defaultValue={label}
-            onChange={(e) => {
-              label = e.target.value;
-              this.setState(() => {
-                return {
-                  newLabel: label,
-                };
-              });
-            }}
-            onClick={(e) => e.stopPropagation()}
-          />
-        </form>
-      </li>
-    );
-  }
+function Task({
+  changeCompleted,
+  completed,
+  editing,
+  dateCreated,
+  editTask,
+  editTaskSubmit,
+  minutes,
+  seconds,
+  updateTime,
+  label,
+  deleteTask,
+}) {
+  const [newLabel, setNewLabel] = useState(null);
+  const taskTimer = <TaskTimer minutes={minutes} seconds={seconds} updateTime={updateTime} />;
+  return (
+    <li className={[editing ? 'editing' : [completed ? 'completed' : '']]}>
+      <div className="view">
+        <input className="toggle" type="checkbox" onChange={changeCompleted} checked={completed} />
+        <label htmlFor="desctiption">
+          <span className="title" role="presentation" onClick={changeCompleted} onKeyDown={changeCompleted}>
+            {label}
+          </span>
+          {!completed ? taskTimer : null}
+          <span className="description">
+            created {formatDistanceToNowStrict(dateCreated, { includeSeconds: true })} ago
+          </span>
+        </label>
+        <button
+          type="button"
+          aria-label="Edit task"
+          className="icon icon-edit"
+          onClick={(e) => {
+            e.stopPropagation();
+            editTask();
+          }}
+        />
+        <button type="button" aria-label="Delete task" className="icon icon-destroy" onClick={() => deleteTask()} />
+      </div>
+      <form onSubmit={(e) => editTaskSubmit(e, newLabel)}>
+        <input
+          type="text"
+          className="edit"
+          defaultValue={label}
+          onChange={(e) => {
+            label = e.target.value;
+            setNewLabel(() => {
+              return {
+                newLabel: label,
+              };
+            });
+          }}
+          onClick={(e) => e.stopPropagation()}
+        />
+      </form>
+    </li>
+  );
 }
+
+Task.defaultProps = {
+  label: 'New task',
+  completed: false,
+  editing: false,
+  dateCreated: new Date(),
+  deleteTask: () => {},
+  editTask: () => {},
+  editTaskSubmit: () => {},
+  changeCompleted: () => {},
+};
+
+Task.propTypes = {
+  label: PropTypes.string,
+  completed: PropTypes.bool,
+  editing: PropTypes.bool,
+  dateCreated: PropTypes.instanceOf(Date),
+  deleteTask: PropTypes.func,
+  editTask: PropTypes.func,
+  editTaskSubmit: PropTypes.func,
+  changeCompleted: PropTypes.func,
+};
+
+// class TaskClass extends Component {
+//   static defaultProps = {
+//     label: 'New task',
+//     completed: false,
+//     editing: false,
+//     dateCreated: new Date(),
+//     deleteTask: () => {},
+//     editTask: () => {},
+//     editTaskSubmit: () => {},
+//     changeCompleted: () => {},
+//   };
+
+//   static propTypes = {
+//     label: PropTypes.string,
+//     completed: PropTypes.bool,
+//     editing: PropTypes.bool,
+//     dateCreated: PropTypes.instanceOf(Date),
+//     deleteTask: PropTypes.func,
+//     editTask: PropTypes.func,
+//     editTaskSubmit: PropTypes.func,
+//     changeCompleted: PropTypes.func,
+//   };
+
+//   state = {
+//     newLabel: null,
+//   };
+
+//   deleteTaskItem = () => {
+//     const { deleteTask } = this.props;
+//     deleteTask();
+//   };
+
+//   render() {
+//     const { changeCompleted, completed, editing, dateCreated, editTask, editTaskSubmit, minutes, seconds, updateTime } =
+//       this.props;
+//     let { label } = this.props;
+//     const { newLabel } = this.state;
+//     const taskTimer = <TaskTimer minutes={minutes} seconds={seconds} updateTime={updateTime} />;
+//     return (
+//       <li className={[editing ? 'editing' : [completed ? 'completed' : '']]}>
+//         <div className="view">
+//           <input className="toggle" type="checkbox" onChange={changeCompleted} checked={completed} />
+//           <label htmlFor="desctiption">
+//             <span className="title" role="presentation" onClick={changeCompleted} onKeyDown={changeCompleted}>
+//               {label}
+//             </span>
+//             {!completed ? taskTimer : null}
+//             <span className="description">
+//               created {formatDistanceToNowStrict(dateCreated, { includeSeconds: true })} ago
+//             </span>
+//           </label>
+//           <button
+//             type="button"
+//             aria-label="Edit task"
+//             className="icon icon-edit"
+//             onClick={(e) => {
+//               e.stopPropagation();
+//               editTask();
+//             }}
+//           />
+//           <button
+//             type="button"
+//             aria-label="Delete task"
+//             className="icon icon-destroy"
+//             onClick={() => this.deleteTaskItem()}
+//           />
+//         </div>
+//         <form onSubmit={(e) => editTaskSubmit(e, newLabel)}>
+//           <input
+//             type="text"
+//             className="edit"
+//             defaultValue={label}
+//             onChange={(e) => {
+//               label = e.target.value;
+//               this.setState(() => {
+//                 return {
+//                   newLabel: label,
+//                 };
+//               });
+//             }}
+//             onClick={(e) => e.stopPropagation()}
+//           />
+//         </form>
+//       </li>
+//     );
+//   }
+// }
 
 export default Task;
