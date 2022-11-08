52fd00b6207faf1b4a26dd725cc8a848a312a0ee
Dmitry Fedorov <dima050595fedorov@gmail.com>
2022-11-09 01:16:39 +0300

    Делаю TaskList

--- src/NewTaskForm/NewTaskForm.js
+++ src/NewTaskForm/NewTaskForm.js
@@ -2,43 +2,63 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const submitNewTask = (e, addNewTask, newTaskName, newMin, newSec) => {
+const submitNewTask = (e, addNewTask, dataTask, setDataTask) => {
   e.preventDefault();
+  const { newTaskName, newMin, newSec } = dataTask;
   const correctName = !!(newTaskName.length !== 0 && newTaskName.trim());
-  const correctMin = !!(+newMin + 1 && +newMin >= 0);
-  const correctSec = !!(+newSec >= 0 && +newSec < 60);
+  const correctMin = !!(+newMin >= 0);
+  const correctSec = !!(((+newMin > 0 && +newSec >= 0) || (+newMin === 0 && +newSec > 0)) && +newSec < 60);
   if (correctName && correctMin && correctSec) {
     addNewTask(newTaskName, newMin, newSec);
+    setDataTask({
+      newTaskName: '',
+      newMin: '',
+      newSec: '',
+    });
   }
 };
 function NewTaskForm({ addNewTask }) {
-  const [newTaskName, setNewTaskName] = useState('');
-  const [newMin, setNewMin] = useState('');
-  const [newSec, setNewSec] = useState('');
+  const [dataTask, setDataTask] = useState({
+    newTaskName: '',
+    newMin: '',
+    newSec: '',
+  });
   return (
     <header className="header">
       <h1>todos</h1>
-      <form onSubmit={(e) => submitNewTask(e, addNewTask, newTaskName, newMin, newSec)} className="new-todo-form">
+      <form onSubmit={(e) => submitNewTask(e, addNewTask, dataTask, setDataTask)} className="new-todo-form">
         <input
           type="text"
           className="new-todo"
           placeholder="What needs to be done?"
-          onChange={(e) => setNewTaskName(() => e.target.value)}
-          value={newTaskName}
+          onChange={(e) =>
+            setDataTask(() => {
+              return { ...dataTask, newTaskName: e.target.value };
+            })
+          }
+          value={dataTask.newTaskName}
         />
         <input
           type="text"
           className="new-todo-form__timer"
           placeholder="Min"
-          onChange={(e) => setNewMin(() => e.target.value)}
-          value={newMin}
+          onChange={(e) =>
+            setDataTask(() => {
+              return { ...dataTask, newMin: e.target.value };
+            })
+          }
+          value={dataTask.newMin}
         />
         <input
           type="text"
           className="new-todo-form__timer"
           placeholder="Sec"
-          onChange={(e) => setNewSec(e.target.value)}
-          value={newSec}
+          onChange={(e) =>
+            setDataTask(() => {
+              return { ...dataTask, newSec: e.target.value };
+            })
+          }
+          value={dataTask.newSec}
         />
         <input className="hidden" type="submit" />
       </form>
@@ -47,108 +67,11 @@ function NewTaskForm({ addNewTask }) {
 }
 
 NewTaskForm.defaultProps = {
-  className: '',
   addNewTask: () => {},
 };
 
 NewTaskForm.propTypes = {
-  className: PropTypes.string,
   addNewTask: PropTypes.func,
 };
-
-// class NewTaskFormClass extends Component {
-//   static defaultProps = {
-//     className: '',
-//     addNewTask: () => {},
-//   };
-
-//   static propTypes = {
-//     className: PropTypes.string,
-//     addNewTask: PropTypes.func,
-//   };
-
-//   state = {
-//     newTaskName: '',
-//     newMin: '',
-//     newSec: '',
-//   };
-
-//   changeNameNewTask = (e) => {
-//     this.setState(() => {
-//       return {
-//         newTaskName: e.target.value,
-//       };
-//     });
-//   };
-
-//   changeMinNewTask = (e) => {
-//     this.setState(() => {
-//       return {
-//         newMin: e.target.value,
-//       };
-//     });
-//   };
-
-//   changeSecNewTask = (e) => {
-//     this.setState(() => {
-//       return {
-//         newSec: e.target.value,
-//       };
-//     });
-//   };
-
-//   submitNewTask = (e) => {
-//     e.preventDefault();
-//     const { addNewTask } = this.props;
-//     const { newTaskName, newMin, newSec } = this.state;
-//     const correctName = !!(newTaskName.length !== 0 && newTaskName.trim());
-//     const correctMin = !!(+newMin + 1 && +newMin >= 0);
-//     const correctSec = !!(+newSec >= 0 && +newSec < 60);
-//     if (correctName && correctMin && correctSec) {
-//       addNewTask(newTaskName, newMin, newSec);
-//       this.setState(() => {
-//         return {
-//           newTaskName: '',
-//           newMin: '',
-//           newSec: '',
-//         };
-//       });
-//     }
-//   };
-
-//   render() {
-//     const { newTaskName, newMin, newSec } = this.state;
-
-//     return (
-//       <header className="header">
-//         <h1>todos</h1>
-//         <form onSubmit={this.submitNewTask} className="new-todo-form">
-//           <input
-//             type="text"
-//             className="new-todo"
-//             placeholder="What needs to be done?"
-//             onChange={this.changeNameNewTask}
-//             value={newTaskName}
-//           />
-//           <input
-//             type="text"
-//             className="new-todo-form__timer"
-//             placeholder="Min"
-//             onChange={this.changeMinNewTask}
-//             value={newMin}
-//           />
-//           <input
-//             type="text"
-//             className="new-todo-form__timer"
-//             placeholder="Sec"
-//             onChange={this.changeSecNewTask}
-//             value={newSec}
-//           />
-//           <input className="hidden" type="submit" />
-//         </form>
-//       </header>
-//     );
-//   }
-// }
 
 export default NewTaskForm;
--- src/TaskList/TaskList.js
+++ src/TaskList/TaskList.js
@@ -1,110 +1,170 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import React, { Component, useState } from 'react';
+// import PropTypes from 'prop-types';
 
-import Task from '../Task';
+// import Task from '../Task';
 
-class TaskList extends Component {
-  static defaultProps = {
-    todoData: [{ label: 'New task', dateCreated: new Date(), completed: false, editing: false, id: 0 }],
-    filterName: 'All',
-    deleteTask: () => {},
-    editTask: () => {},
-    editTaskSubmit: () => {},
-    changeCompleted: () => {},
-  };
+// addElement = (todoData) => {
+//   const { deleteTask, changeCompleted, editTaskSubmit, updateTime } = this.props;
+//   const { editing, editingLabel } = this.state;
+//   return todoData.map((item) => {
+//     const { id, ...newItem } = { ...item };
+//     return (
+//       <Task
+//         key={id}
+//         {...newItem}
+//         id={id}
+//         editing={id === editing}
+//         editingLabel={editingLabel}
+//         changeCompleted={() => changeCompleted(id)}
+//         deleteTask={() => deleteTask(id)}
+//         editTask={() => this.editTask(id)}
+//         editTaskSubmit={(e, newLabel) => this.editTaskSubmits(e, newLabel, todoData, id, editTaskSubmit)}
+//         updateTime={(newSeconds) => updateTime(newSeconds, id)}
+//       />
+//     );
+//   });
+// };
 
-  static propTypes = {
-    todoData: PropTypes.arrayOf(PropTypes.object),
-    filterName: PropTypes.string,
-    deleteTask: PropTypes.func,
-    editTask: PropTypes.func,
-    editTaskSubmit: PropTypes.func,
-    changeCompleted: PropTypes.func,
-  };
+// const filterElements = (todoData, filterName) => {
+//   const activeElements = todoData.filter((item) => !item.completed);
+//   const complitedElements = todoData.filter((item) => item.completed);
+//   let elements;
 
-  state = {
-    editing: null,
-  };
+//   if (filterName === 'All') {
+//     elements = addElement(todoData);
+//   } else if (filterName === 'Active') {
+//     elements = addElement(activeElements);
+//   } else {
+//     elements = addElement(complitedElements);
+//   }
+//   return elements;
+// };
 
-  handleClick = () => {
-    document.removeEventListener('click', this.handleClick);
-    this.setState(() => {
-      return {
-        editing: null,
-      };
-    });
-  };
+// function TaskList(todoData, deleteTask, editTask, editTaskSubmit, changeCompleted, filterName, updateTime) {
+//   const elements = filterElements(todoData, filterName);
+//   return <ul className="todo-list">{elements}</ul>;
+// }
 
-  addElement = (todoData) => {
-    const { deleteTask, changeCompleted, editTaskSubmit, updateTime } = this.props;
-    const { editing, editingLabel } = this.state;
-    return todoData.map((item) => {
-      const { id, ...newItem } = { ...item };
-      return (
-        <Task
-          key={id}
-          {...newItem}
-          id={id}
-          editing={id === editing}
-          editingLabel={editingLabel}
-          changeCompleted={() => changeCompleted(id)}
-          deleteTask={() => deleteTask(id)}
-          editTask={() => this.editTask(id)}
-          editTaskSubmit={(e, newLabel) => this.editTaskSubmits(e, newLabel, todoData, id, editTaskSubmit)}
-          updateTime={(newSeconds) => updateTime(newSeconds, id)}
-        />
-      );
-    });
-  };
+// TaskList.defaultProps = {
+//   todoData: [{ label: 'New task', dateCreated: new Date(), completed: false, editing: false, id: 0 }],
+//   filterName: 'All',
+//   deleteTask: () => {},
+//   editTask: () => {},
+//   editTaskSubmit: () => {},
+//   changeCompleted: () => {},
+// };
 
-  filterElements = () => {
-    const { todoData, filterName } = this.props;
-    const activeElements = todoData.filter((item) => !item.completed);
-    const complitedElements = todoData.filter((item) => item.completed);
-    let elements;
+// TaskList.propTypes = {
+//   todoData: PropTypes.arrayOf(PropTypes.object),
+//   filterName: PropTypes.string,
+//   deleteTask: PropTypes.func,
+//   editTask: PropTypes.func,
+//   editTaskSubmit: PropTypes.func,
+//   changeCompleted: PropTypes.func,
+// };
 
-    if (filterName === 'All') {
-      elements = this.addElement(todoData);
-    } else if (filterName === 'Active') {
-      elements = this.addElement(activeElements);
-    } else {
-      elements = this.addElement(complitedElements);
-    }
-    return elements;
-  };
+// class TaskListClass extends Component {
+//   // static defaultProps = {
+//   //   todoData: [{ label: 'New task', dateCreated: new Date(), completed: false, editing: false, id: 0 }],
+//   //   filterName: 'All',
+//   //   deleteTask: () => {},
+//   //   editTask: () => {},
+//   //   editTaskSubmit: () => {},
+//   //   changeCompleted: () => {},
+//   // };
 
-  editTaskSubmits(e, newLabel, todoData, id, editTaskSubmit) {
-    e.preventDefault();
-    document.removeEventListener('click', this.handleClick);
-    this.setState(() => {
-      return {
-        editing: null,
-        editingLabel: null,
-      };
-    });
-    if (newLabel) {
-      const idx = todoData.findIndex((item) => item.id === id);
-      const changeItem = todoData[idx];
-      changeItem.label = newLabel;
-      changeItem.editing = false;
-      const newData = [...todoData.slice(0, idx), changeItem, ...todoData.slice(idx + 1)];
-      editTaskSubmit(newData);
-    }
-  }
+//   // static propTypes = {
+//   //   todoData: PropTypes.arrayOf(PropTypes.object),
+//   //   filterName: PropTypes.string,
+//   //   deleteTask: PropTypes.func,
+//   //   editTask: PropTypes.func,
+//   //   editTaskSubmit: PropTypes.func,
+//   //   changeCompleted: PropTypes.func,
+//   // };
 
-  editTask(id) {
-    document.addEventListener('click', this.handleClick);
-    this.setState(() => {
-      return {
-        editing: id,
-      };
-    });
-  }
+//   state = {
+//     editing: null,
+//   };
 
-  render() {
-    const elements = this.filterElements();
-    return <ul className="todo-list">{elements}</ul>;
-  }
-}
+//   handleClick = () => {
+//     document.removeEventListener('click', this.handleClick);
+//     this.setState(() => {
+//       return {
+//         editing: null,
+//       };
+//     });
+//   };
 
-export default TaskList;
+//   // addElement = (todoData) => {
+//   //   const { deleteTask, changeCompleted, editTaskSubmit, updateTime } = this.props;
+//   //   const { editing, editingLabel } = this.state;
+//   //   return todoData.map((item) => {
+//   //     const { id, ...newItem } = { ...item };
+//   //     return (
+//   //       <Task
+//   //         key={id}
+//   //         {...newItem}
+//   //         id={id}
+//   //         editing={id === editing}
+//   //         editingLabel={editingLabel}
+//   //         changeCompleted={() => changeCompleted(id)}
+//   //         deleteTask={() => deleteTask(id)}
+//   //         editTask={() => this.editTask(id)}
+//   //         editTaskSubmit={(e, newLabel) => this.editTaskSubmits(e, newLabel, todoData, id, editTaskSubmit)}
+//   //         updateTime={(newSeconds) => updateTime(newSeconds, id)}
+//   //       />
+//   //     );
+//   //   });
+//   // };
+
+//   // filterElements = () => {
+//   //   const { todoData, filterName } = this.props;
+//   //   const activeElements = todoData.filter((item) => !item.completed);
+//   //   const complitedElements = todoData.filter((item) => item.completed);
+//   //   let elements;
+
+//   //   if (filterName === 'All') {
+//   //     elements = this.addElement(todoData);
+//   //   } else if (filterName === 'Active') {
+//   //     elements = this.addElement(activeElements);
+//   //   } else {
+//   //     elements = this.addElement(complitedElements);
+//   //   }
+//   //   return elements;
+//   // };
+
+//   editTaskSubmits(e, newLabel, todoData, id, editTaskSubmit) {
+//     e.preventDefault();
+//     document.removeEventListener('click', this.handleClick);
+//     this.setState(() => {
+//       return {
+//         editing: null,
+//         editingLabel: null,
+//       };
+//     });
+//     if (newLabel) {
+//       const idx = todoData.findIndex((item) => item.id === id);
+//       const changeItem = todoData[idx];
+//       changeItem.label = newLabel;
+//       changeItem.editing = false;
+//       const newData = [...todoData.slice(0, idx), changeItem, ...todoData.slice(idx + 1)];
+//       editTaskSubmit(newData);
+//     }
+//   }
+
+//   editTask(id) {
+//     document.addEventListener('click', this.handleClick);
+//     this.setState(() => {
+//       return {
+//         editing: id,
+//       };
+//     });
+//   }
+
+//   // render() {
+//   //   const elements = this.filterElements();
+//   //   return <ul className="todo-list">{elements}</ul>;
+//   // }
+// }
+
+// export default TaskList;
